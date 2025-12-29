20fda670f02c278658b960f573ed87fbaa430ea3
Luis <luisrz09@gmail.com>
2025-12-29 09:51:28 -0500

    📂 forzar inclusion de uploads

--- chatbot-dashboard/src/components/ConnectWhatsAppTwilioEmbeddedSignupButton.tsx
+++ chatbot-dashboard/src/components/ConnectWhatsAppTwilioEmbeddedSignupButton.tsx
@@ -9,7 +9,7 @@ type Props = {
 };
 
 const META_APP_ID = '672113805196816';
-const CONFIG_ID = '4374739139515059';
+const CONFIG_ID = '859575230051675';
 const SOLUTION_ID = process.env.NEXT_PUBLIC_TWILIO_PARTNER_SOLUTION_ID;
 
 export default function ConnectWhatsAppTwilioEmbeddedSignupButton({
